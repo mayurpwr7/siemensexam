274b012332e748779193250c738df7f9fc95881f
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2025-03-19 15:14:21 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "lambda_sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  tags = {
+    Name = "lambda-security-group"
+  }
+}
