21bdbd10b584ac6ce04c1c35c0ba28b852bd07ef
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2025-03-19 15:14:59 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"  # You can change this CIDR as needed
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
