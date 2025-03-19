2acd5ab2dd3af2a75b0cd15d9165c95ad6e817cc
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2025-03-19 15:32:43 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,26 +1,18 @@
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "iam:CreateRole",
-        "iam:CreatePolicy",
-        "iam:AttachRolePolicy",
-        "iam:PutRolePolicy",
-        "ec2:CreateVpc",
-        "ec2:CreateSubnet",
-        "ec2:CreateRouteTable",
-        "ec2:CreateSecurityGroup",
-        "lambda:CreateFunction",
-        "lambda:AddPermission",
-        "apigateway:POST"
-      ],
-      "Resource": "*"
-    }
-  ]
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
-
 
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda_policy"
@@ -51,3 +43,4 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
+
