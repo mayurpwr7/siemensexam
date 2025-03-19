bd79ac099518af273eb16bc399ac02717f713487
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2025-03-19 17:00:45 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "main_vpc" {
 
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.2.0/24"  # You can change this CIDR as needed
+  cidr_block              = "10.0.21.0/24"  # You can change this CIDR as needed
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
   tags = {
