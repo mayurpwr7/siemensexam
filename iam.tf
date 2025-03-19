2167ce99bb138359c5ab49f344370e3e55c026e3
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2025-03-19 15:10:37 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "Lambda Policy with basic execution permissions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["logs:*"]
+        Effect   = "Allow"
+        Resource = "arn:aws:logs:*:*:*"
+      },
+      {
+        Action   = ["lambda:InvokeFunction"]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action   = ["apigateway:POST"]
+        Effect   = "Allow"
+        Resource = "arn:aws:apigateway:eu-west-1::/restapis/*/resources/*/methods/POST"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
